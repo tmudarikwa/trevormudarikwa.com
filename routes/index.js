7ef46ee9ae0eaacd9438888abe3c74ae09f9b980
Nymud <nymud94@gmail.com>
2021-01-29 22:29:07 +0200

    changes to title

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ router.get('/', function(req, res, next) {
   var path = '/';
   res.locals.path = path;
   res.locals.year = date.getFullYear();
-  res.render('index', { title: 'Takunda Mudarikwa - Full-stack Web Developer | HOME', year: date.getFullYear()});
+  res.render('index', { title: 'Trevor Mudarikwa - Front-End Web Developer ', year: date.getFullYear()});
 });
 
 module.exports = router;
